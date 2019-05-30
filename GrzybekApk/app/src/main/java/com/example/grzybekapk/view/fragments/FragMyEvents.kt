d602b22b4a6bf8fc703f37e6890cf0e861f48621
mnehrebecki <mati9817@gmail.com>
2019-05-30 20:44:34 +0200

    New list layout

--- GrzybekApk/app/src/main/java/com/example/grzybekapk/view/fragments/FragMyEvents.kt
+++ GrzybekApk/app/src/main/java/com/example/grzybekapk/view/fragments/FragMyEvents.kt
@@ -5,20 +5,25 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
 import com.crashlytics.android.Crashlytics
 import com.example.grzybekapk.R
 import com.example.grzybekapk.view.DataForEvents
+import com.example.grzybekapk.view.Event
+import com.example.grzybekapk.view.EventsAdapter
 import com.example.grzybekapk.view.activities.EventDetailsActivity
 import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_my_events.*
+import kotlinx.android.synthetic.main.fragment_start_screen.*
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
@@ -39,9 +44,9 @@ class FragMyEvents: Fragment() {
 
         val eventsList = ArrayList<DataForEvents>() //Array for DataForEvents objects
         val eventsArray = ArrayList<String>()   // Array for event name and date for list view
-
-        val arrayAdapter = ArrayAdapter(activity,android.R.layout.simple_list_item_1,eventsArray)
-        myeventsListView.adapter = arrayAdapter
+        val events = ArrayList<Event>()
+        val myEventsAdapter = EventsAdapter(events)
+        myEventsRecyclerView.layoutManager = LinearLayoutManager(activity, LinearLayout.VERTICAL,false)
 
         val local = Locale("pol") // localize date
         val dateFormat2  = SimpleDateFormat("dd.MM.YYYY HH:mm",local) // short date format
@@ -66,9 +71,10 @@ class FragMyEvents: Fragment() {
                         calendar as Calendar,
                         document.data["Owner"] as String
                     )
-                    eventsArray.add(dateFormat2.format(date.toDate()) as String + "    " + document.data["Name"] as String)
+                    events.add(Event(document.data["Name"].toString(),dateFormat2.format(date.toDate()) as String,document.data["Desc"].toString()))
                     eventsList.add(nextEvent)
-                    arrayAdapter.notifyDataSetChanged()
+                    myEventsRecyclerView.adapter = myEventsAdapter
+                    myEventsAdapter.notifyDataSetChanged()
 
                     Log.d(Crashlytics.TAG, "${document.id} => ${document.data}")
                 }
@@ -77,11 +83,13 @@ class FragMyEvents: Fragment() {
                 Log.w(Crashlytics.TAG, "Error getting documents: ", exception)
             }
 
-        myeventsListView.onItemClickListener = AdapterView.OnItemClickListener{adapterView,view,i,l->
-            val intent = Intent(activity,EventDetailsActivity::class.java)
-            intent.putExtra("event",eventsList[i])
-
-            startActivity(intent)
+        myEventsAdapter.setOnItemClickListener(object : EventsAdapter.ClickListener {
+            override fun onClick(pos: Int, aView: View) {
+                val intent = Intent(activity,EventDetailsActivity::class.java)
+                intent.putExtra("event",eventsList[pos])
+                startActivity(intent)
+            }
         }
+        )
     }
 }
